86910fb943a1c9bdb8df8dc01b09c96d8de37953
ajo.macias@yavirac.edu.ec <ajo.macias@yavirac.edu.ecuser.nameajomacias>
2021-10-02 04:21:45 -0500

    ________________

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ const sequelize = require('./database/db')
 const app = express();
 const PORT = process.env.PORT || '4000';
 
-app.use("/",indexRouter);
+app.use("/", indexRouter);
 
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 
-        sequelize.sync({force: false}).then(() => {
-            console.log("siiii")
-        }).catch((err) => {
-            console.log("error",err);
-        })
-       
+    sequelize.sync({ force: false }).then(() => {
+        console.log("siiii")
+    }).catch((err) => {
+        console.log("error", err);
+    })
+
 });
